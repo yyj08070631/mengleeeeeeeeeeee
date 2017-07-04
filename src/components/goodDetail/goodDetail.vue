df1c3b662d0237130767a9a43674a32ef5cf29a6
YYJ <934780820@qq.com>
2017-07-04 18:06:24 +0800

    mengle

---
+++ src/components/goodDetail/goodDetail.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="niubi">牛逼啊</div>
+</template>
+<script type="ecmascript-6">
+export default {
+  
+}
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+	@import '../../commom/stylus/mixin'
+
+	.niubi
+		position absolute
+		top 50%
+		left 50%
+</style>
