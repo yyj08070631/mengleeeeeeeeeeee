b1b972136172bdf17cc2532dd247d2017f0726f6
YYJ <934780820@qq.com>
2017-07-08 10:28:49 +0800

    mengle

--- src/components/goodDetail/goodDetail.vue
+++ src/components/goodDetail/goodDetail.vue
@@ -14,7 +14,7 @@
 		<!--图片轮播-->
 		<swiper :aspect-ratio="640/800" loop auto @on-index-change="onIndexChange">
 			<swiper-item class="swiper-demo-img" v-for="(item, index) in bannerImg" :key="index">
-				<img :src="item" width="100%">
+				<img :src="item" width="100%" height="100%">
 			</swiper-item>
 		</swiper>
 		<!--导航按钮-->
@@ -263,8 +263,7 @@ export default {
 					display flex
 					align-items center
 					justify-content center
-					border 1px solid #f6f6f6
-					border-right 0
+					border-left 1px solid #e0e0e0
 					font-size 0.6875rem
 				a:first-child
 					border-left 0
