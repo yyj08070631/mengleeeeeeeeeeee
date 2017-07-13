a2b1d26a9ff4b01abe21567b32783652feb31a95
YYJ <934780820@qq.com>
2017-07-13 18:06:28 +0800

    mengle

--- src/components/offlineInfo/offlineInfo.vue
+++ src/components/offlineInfo/offlineInfo.vue
@@ -18,23 +18,7 @@
         <!-- 主体 -->
         <section class="main">
             <!--图片轮播-->
-            <swiper :options="swiperOption" ref="mySwiper">
-                <!-- 这部分放你要渲染的那些内容 -->
-                <!-- <swiper-slide v-for="(img, index) in imgList">
-                            <img :src=imgList[index] alt="">
-                        </swiper-slide> -->
-                <swiper-slide>
-                    <img src="http://www.pptbz.com/d/file/p/201701/smallffd423a3ab9e735686ff2f099adfe309.jpg" alt="">
-                </swiper-slide>
-                <swiper-slide>
-                    <img src="http://img1.gamedog.cn/2011/11/22/14-111122102315-50.jpg" alt="">
-                </swiper-slide>
-                <swiper-slide>
-                    <img src="http://www.pptbz.com/d/file/p/201701/small70188852ba4be2e23b26ddb7b14df6f3.jpg" alt="">
-                </swiper-slide>
-                <!-- 这是轮播的小圆点 -->
-                <div class="swiper-pagination" slot="pagination"></div>
-            </swiper>
+            <mySwiper class="mySwiper"></mySwiper>
             <!-- 线下信息 -->
             <div class="offlineInfo">
                 <div class="rowUp">
@@ -189,8 +173,8 @@
     </div>
 </template>
 <script type="ecmascript-6">
-import { swiper, swiperSlide } from 'vue-awesome-swiper'
 import { Spinner } from 'vux'
+import mySwiper from '../mySwiper/mySwiper'
 
 const imgList = [
     'http://www.pptbz.com/d/file/p/201701/smallffd423a3ab9e735686ff2f099adfe309.jpg',
@@ -200,66 +184,8 @@ const imgList = [
 
 export default {
     components: {
-        swiper,
-        swiperSlide,
-        Spinner
-    },
-    props: {
-        imgList: [
-            'http://www.pptbz.com/d/file/p/201701/smallffd423a3ab9e735686ff2f099adfe309.jpg',
-            'http://img1.gamedog.cn/2011/11/22/14-111122102315-50.jpg',
-            'http://www.pptbz.com/d/file/p/201701/small70188852ba4be2e23b26ddb7b14df6f3.jpg'
-        ]
-    },
-    data() {
-        return {
-            swiperOption: {
-                // NotNextTick is a component's own property, and if notNextTick is set to true, the component will not instantiate the swiper through NextTick, which means you can get the swiper object the first time (if you need to use the get swiper object to do what Things, then this property must be true)
-                // notNextTick是一个组件自有属性，如果notNextTick设置为true，组件则不会通过NextTick来实例化swiper，也就意味着你可以在第一时间获取到swiper对象，假如你需要刚加载遍使用获取swiper对象来做什么事，那么这个属性一定要是true
-                notNextTick: true,
-                // swiper configs 所有的配置同swiper官方api配置
-                autoplay: 3000,
-                // direction: 'vertical',
-                // effect:"coverflow",
-                // grabCursor : true,
-                // setWrapperSize :true,
-                // autoHeight: true,
-                // loop : true,
-                paginationType: "bullets",
-                pagination: '.swiper-pagination',
-                paginationBulletRender: function (swiper, index, className) {
-                    let width = (100/swiper.imagesLoaded) + '%';
-                    console.log(width);
-                    return '<span class="' + className + '"' + 'style="width:'+ width +'"' + '></span>';
-                }
-                // paginationClickable :true,
-                // prevButton:'.swiper-button-prev',
-                // nextButton:'.swiper-button-next',
-                // scrollbar:'.swiper-scrollbar',
-                // mousewheelControl : true,
-                // observeParents:true,
-                // if you need use plugins in the swiper, you can config in here like this
-                // 如果自行设计了插件，那么插件的一些配置相关参数，也应该出现在这个对象中，如下debugger
-                // debugger: true,
-                // swiper callbacks
-                // swiper的各种回调函数也可以出现在这个对象中，和swiper官方一样
-                // onTransitionStart(swiper){
-                //   console.log(swiper)
-                // },
-                // more Swiper configs and callbacks...
-                // ...
-            }
-        }
-    },
-    //定义这个sweiper对象  
-    computed: {
-        swiper() {
-            return this.$refs.mySwiper.swiper;
-        }
-    },
-    mounted() {
-        //这边就可以使用swiper这个对象去使用swiper官网中的那些方法  
-        // this.swiper.slideTo(0, 0, false);
+        Spinner,
+        'mySwiper': mySwiper
     }
 }
 </script>
