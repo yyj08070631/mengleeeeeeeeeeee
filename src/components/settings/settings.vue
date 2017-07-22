a2610bb27db17b1508733d83b62aa58fb71a09d8
大帅比 <934780820@qq.com>
2017-07-22 16:25:03 +0800

    d

--- src/components/settings/settings.vue
+++ src/components/settings/settings.vue
@@ -23,6 +23,7 @@
             </div>
         </div>
         <div class="content-wrapper">
+            <!-- 我的二维码 -->
             <a href="#myQRcode" class="message-item">
                 <span class="title">我的二维码</span>
                 <div class="link-wrapper">
@@ -30,13 +31,15 @@
                     <img class="other" src="./more.png">
                 </div>
             </a>
+            <!-- 昵称 -->
             <a href="javascript:void(0)" class="message-item">
                 <span class="title">昵称</span>
                 <div class="link-wrapper">
-                    <span class="msg">{{data.username}}</span>
+                    <input class="inputBox msg" @change="changeNickname($event)" :value="data.username">
                     <img class="other" src="./more.png">
                 </div>
             </a>
+            <!-- 收件信息 -->
             <a href="#addrManage" class="message-item indent">
                 <span class="title">收件信息</span>
                 <div class="link-wrapper">
@@ -45,81 +48,150 @@
                 </div>
             </a>
             <div class="line"></div>
+            <!-- 星座 -->
             <a href="javascript:void(0)" class="message-item">
                 <span class="title">星座</span>
                 <div class="link-wrapper">
-                    <span class="msg">{{data.constellation}}</span>
+                    <select class="selectBox msg" @change="changeConstellation($event)" :value="data.constellation">
+                        <option value="1">白羊座</option>
+                        <option value="2">金牛座</option>
+                        <option value="3">双子座</option>
+                        <option value="4">巨蟹座</option>
+                        <option value="5">狮子座</option>
+                        <option value="6">处女座</option>
+                        <option value="7">天秤座</option>
+                        <option value="8">天蝎座</option>
+                        <option value="9">射手座</option>
+                        <option value="10">摩羯座</option>
+                        <option value="11">水瓶座</option>
+                        <option value="12">双鱼座</option>
+                    </select>
                     <img class="other" src="./more.png">
                 </div>
-                <!-- 天才如我才能想出来的透明遮罩！ -->
-                <select class="constellationBra" @change="changeConstellation($event)">
-                    <option value="1">白羊座</option>
-                    <option value="2">金牛座</option>
-                    <option value="3">双子座</option>
-                    <option value="4">巨蟹座</option>
-                    <option value="5">狮子座</option>
-                    <option value="6">处女座</option>
-                    <option value="7">天秤座</option>
-                    <option value="8">天蝎座</option>
-                    <option value="9">射手座</option>
-                    <option value="10">摩羯座</option>
-                    <option value="11">水瓶座</option>
-                    <option value="12">双鱼座</option>
-                </select>
             </a>
+            <!-- 身高 -->
             <a href="javascript:void(0)" class="message-item">
                 <span class="title">身高</span>
                 <div class="link-wrapper">
-                    <span class="msg">{{data.height}}cm</span>
+                    <div class="inputBox msg">
+                        <input class="msg" @change="changeHeight($event)" :value="data.height">
+                        <span class="msg">cm</span>
+                    </div>
                     <img class="other" src="./more.png">
                 </div>
             </a>
+            <!-- 体重 -->
             <a href="javascript:void(0)" class="message-item indent">
                 <span class="title">体重</span>
                 <div class="link-wrapper">
-                    <span class="msg">{{data.weight}}kg</span>
+                    <div class="inputBox msg">
+                        <input class="msg" @change="changeWeight($event)" :value="data.weight">
+                        <span class="msg">kg</span>
+                    </div>
                     <img class="other" src="./more.png">
                 </div>
             </a>
             <div class="line"></div>
+            <!-- 职业 -->
             <a href="javascript:void(0)" class="message-item">
                 <span class="title">职业</span>
                 <div class="link-wrapper">
-                    <span class="msg">{{data.career}}</span>
+                    <select class="selectBox msg" @change="changeCareer($event)" :value="data.career">
+                        <option value="销售业务">销售业务</option>
+                        <option value="销售管理">销售管理</option>
+                        <option value="销售行政/商务">销售行政/商务</option>
+                        <option value="客服/售前/售后技术支持">客服/售前/售后技术支持</option>
+                        <option value="市场">市场</option>
+                        <option value="公关/媒介">公关/媒介</option>
+                        <option value="广告/会展">广告/会展</option>
+                        <option value="财务/审计/税务">财务/审计/税务</option>
+                        <option value="人力资源">人力资源</option>
+                        <option value="行政/后勤/文秘">行政/后勤/文秘</option>
+                        <option value="项目管理/项目协调">项目管理/项目协调</option>
+                        <option value="质量管理/安全防护">质量管理/安全防护</option>
+                        <option value="高级管理">高级管理</option>
+                        <option value="软件/互联网开发/系统集成">软件/互联网开发/系统集成</option>
+                        <option value="硬件开发">硬件开发</option>
+                        <option value="互联网产品/运营管理">互联网产品/运营管理</option>
+                        <option value="IT质量管理/测试/配置管理">IT质量管理/测试/配置管理</option>
+                        <option value="IT运维/技术支持">IT运维/技术支持</option>
+                        <option value="IT管理/项目协调">IT管理/项目协调</option>
+                        <option value="电信/通信技术开发及应用">电信/通信技术开发及应用</option>
+                        <option value="房地产开发/经纪/中介">房地产开发/经纪/中介</option>
+                        <option value="土木/建筑/装修/市政工程">土木/建筑/装修/市政工程</option>
+                        <option value="物业管理">物业管理</option>
+                        <option value="银行">银行</option>
+                        <option value="证券/期货/投资管理/服务">证券/期货/投资管理/服务</option>
+                        <option value="保险">保险</option>
+                        <option value="信托/担保/拍卖/典当">信托/担保/拍卖/典当</option>
+                        <option value="采购/贸易">采购/贸易</option>
+                        <option value="交通运输服务">交通运输服务</option>
+                        <option value="物流/仓储">物流/仓储</option>
+                        <option value="生产管理/运营">生产管理/运营</option>
+                        <option value="电子/电器/半导体/仪器仪表">电子/电器/半导体/仪器仪表</option>
+                        <option value="汽车制造">汽车制造</option>
+                        <option value="汽车销售与服务">汽车销售与服务</option>
+                        <option value="机械设计/制造/维修">机械设计/制造/维修</option>
+                        <option value="服装/纺织/皮革设计/生产">服装/纺织/皮革设计/生产</option>
+                        <option value="技工/操作工">技工/操作工</option>
+                        <option value="生物/制药/医疗器械">生物/制药/医疗器械</option>
+                        <option value="化工">化工</option>
+                        <option value="影视/媒体/出版/印刷">影视/媒体/出版/印刷</option>
+                        <option value="艺术/设计">艺术/设计</option>
+                        <option value="咨询/顾问/调研/数据分析">咨询/顾问/调研/数据分析</option>
+                        <option value="教育/培训">教育/培训</option>
+                        <option value="律师/法务/合规">律师/法务/合规</option>
+                        <option value="翻译（口译与笔译）">翻译（口译与笔译）</option>
+                        <option value="商超/酒店/娱乐管理/服务">商超/酒店/娱乐管理/服务</option>
+                        <option value="旅游/度假/出入境服务">旅游/度假/出入境服务</option>
+                        <option value="烹饪/料理/食品研发">烹饪/料理/食品研发</option>
+                        <option value="保健/美容/美发/健身">保健/美容/美发/健身</option>
+                        <option value="医院/医疗/护理">医院/医疗/护理</option>
+                        <option value="社区/居民/家政服务">社区/居民/家政服务</option>
+                        <option value="能源/矿产/地质勘查">能源/矿产/地质勘查</option>
+                        <option value="环境科学/环保">环境科学/环保</option>
+                        <option value="农/林/牧/渔业">农/林/牧/渔业</option>
+                        <option value="公务员/事业单位/科研机构">公务员/事业单位/科研机构</option>
+                        <option value="实习生/培训生/储备干部">实习生/培训生/储备干部</option>
+                        <option value="志愿者/社会工作者">志愿者/社会工作者</option>
+                        <option value="其他">其他</option>
+                    </select>
                     <img class="other" src="./more.png">
                 </div>
             </a>
+            <!-- 收入 -->
             <a href="javascript:void(0)" class="message-item">
                 <span class="title">收入</span>
                 <div class="link-wrapper">
-                    <span class="msg">{{data.gain}}</span>
+                    <div class="inputBox msg">
+                        <input class="msg" @change="changeGain($event)" :value="data.gain">
+                        <span class="msg">/月</span>
+                    </div>
                     <img class="other" src="./more.png">
                 </div>
             </a>
+            <!-- 兴趣爱好 -->
             <a href="javascript:void(0)" class="message-item">
                 <span class="title">兴趣爱好</span>
                 <div class="link-wrapper">
-                    <span class="msg">{{data.habbit}}</span>
+                    <input class="inputBox msg" @change="changeHabbit($event)" :value="data.habbit">
                     <img class="other" src="./more.png">
                 </div>
             </a>
         </div>
     </div>
 </template>
- <script type="ecmascript-6">
-import { XDialog, TransferDomDirective as TransferDom } from 'vux'
+<script type="ecmascript-6">
 
 export default {
-    directives: {
-        TransferDom
-    },
     components: {
-        XDialog,
+
     },
     data() {
         return {
             data: [],
             showHideOnBlur: true,
+            show: true,
         }
     },
     created() {
@@ -148,12 +220,145 @@ export default {
                 let res = response.body;
                 // console.log($event.target.value)
                 if (res.code == 200) {
-                    
+                    alert('修改成功！！');
                 } else {
                     alert('修改失败！！');
                     return
                 }
             })
+        },
+        // 修改身高
+        changeHeight: function ($event) {
+            // console.log($event.target.value)
+            if ((/^(\+|-)?\d+$/.test($event.target.value)) && $event.target.value > 100 && $event.target.value < 230) {
+                this.$http({
+                    method: 'get',
+                    url: global.Domain + '/user/userSet?userId===tPtcNLZARXEuvDhRSFGkQX&height=' + $event.target.value,
+                    emulateJSON: true
+                }).then(function (response) {
+                    let res = response.body;
+                    // console.log($event.target.value)
+                    if (res.code == 200) {
+                        alert('修改成功！！');
+                    } else {
+                        alert('修改失败！！');
+                        return
+                    }
+                })
+            } else {
+                alert('请输入正确的身高数字！！');
+                return
+            }
+        },
+        // 修改体重
+        changeWeight: function ($event) {
+            // console.log($event.target.value)
+            if ((/^(\+|-)?\d+$/.test($event.target.value)) && $event.target.value > 20 && $event.target.value < 255) {
+                this.$http({
+                    method: 'get',
+                    url: global.Domain + '/user/userSet?userId===tPtcNLZARXEuvDhRSFGkQX&weight=' + $event.target.value,
+                    emulateJSON: true
+                }).then(function (response) {
+                    let res = response.body;
+                    // console.log($event.target.value)
+                    if (res.code == 200) {
+                        alert('修改成功！！');
+                    } else {
+                        alert('修改失败！！');
+                        return
+                    }
+                })
+            } else {
+                alert('请输入正确的体重数字！！');
+                return
+            }
+        },
+        // 修改昵称
+        changeNickname: function ($event) {
+            // console.log($event.target.value.length)
+            if ((/^[\u4E00-\u9FA5A-Za-z0-9_]+$/.test($event.target.value)) && $event.target.value.length >= 3 && $event.target.value.length <= 15) {
+                this.$http({
+                    method: 'get',
+                    url: global.Domain + '/user/userSet?userId===tPtcNLZARXEuvDhRSFGkQX&username=' + $event.target.value,
+                    emulateJSON: true
+                }).then(function (response) {
+                    let res = response.body;
+                    // console.log($event.target.value)
+                    if (res.code == 200) {
+                        alert('修改成功！！');
+                    } else {
+                        alert('修改失败！！');
+                        return
+                    }
+                })
+            } else {
+                alert('昵称仅包含汉字、数字、字母及下划线且长度为3-15个字符！！');
+                return
+            }
+        },
+        // 修改职业
+        changeCareer: function ($event) {
+            // console.log($event.target.value)
+            this.$http({
+                method: 'get',
+                url: global.Domain + '/user/userSet?userId===tPtcNLZARXEuvDhRSFGkQX&career=' + $event.target.value,
+                emulateJSON: true
+            }).then(function (response) {
+                let res = response.body;
+                // console.log($event.target.value)
+                if (res.code == 200) {
+                    alert('修改成功！！');
+                } else {
+                    alert('修改失败！！');
+                    return
+                }
+            })
+        },
+        // 修改收入
+        changeGain: function ($event) {
+            // console.log($event.target.value)
+            if ((/^(\+|-)?\d+$/.test($event.target.value)) && $event.target.value < 4294967295) {
+                this.$http({
+                    method: 'get',
+                    url: global.Domain + '/user/userSet?userId===tPtcNLZARXEuvDhRSFGkQX&gain=' + $event.target.value,
+                    emulateJSON: true
+                }).then(function (response) {
+                    let res = response.body;
+                    // console.log($event.target.value)
+                    if (res.code == 200) {
+                        alert('修改成功！！');
+                    } else {
+                        alert('修改失败！！');
+                        return
+                    }
+                })
+            } else {
+                alert('请输入正确的月收入！！');
+                return
+            }
+        },
+        // 修改兴趣爱好
+        changeHabbit: function ($event) {
+            // console.log($event.target.value.length)
+            if ((/^[\u4E00-\u9FA5A-Za-z]+$/.test($event.target.value)) && $event.target.value.length >= 1 && $event.target.value.length <= 15) {
+                this.$http({
+                    method: 'get',
+                    url: global.Domain + '/user/userSet?userId===tPtcNLZARXEuvDhRSFGkQX&habbit=' + $event.target.value,
+                    emulateJSON: true
+                }).then(function (response) {
+                    let res = response.body;
+                    // console.log($event.target.value)
+                    if (res.code == 200) {
+                        alert('修改成功！！');
+                    } else {
+                        alert('修改失败！！');
+                        return
+                    }
+                })
+            } else {
+                alert('兴趣爱好仅包含汉字、字母！！');
+                return
+            }
         }
     },
     mounted() {
@@ -183,7 +388,25 @@ export default {
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
 @import '../../commom/stylus/mixin'
+    // init
     width = 100%
+    select {   
+        /*Chrome和Firefox里面的边框是不一样的，所以复写了一下*/  
+        border: solid 1px #000;   
+        /*很关键：将默认的select选择框样式清除*/  
+        appearance:none;   
+        -moz-appearance:none;   
+        -webkit-appearance:none;   
+        /*在选择框的最右侧中间显示小箭头图片*/  
+        background: url("http://ourjs.github.io/static/2015/arrow.png") no-repeat scroll rightright center transparent;   
+        /*为下拉小箭头留出一点位置，避免被文字覆盖*/  
+        padding-right: 14px;   
+    }   
+    /*清除ie的默认选择框样式清除，隐藏下拉箭头*/  
+    select::-ms-expand {
+        display: none;
+    }
+    
     .settings-wrapper
         width: 100%
         height: 100%
@@ -275,18 +498,39 @@ export default {
                         margin-top: 0.5rem 
                         width: 0.375rem
                         height: 0.375rem
-                // 天才如我才能想出来的遮罩！
-                .constellationBra
+                // 其实并不是天才如我。。。
+                .selectBox
                     position absolute
                     right 0
-                    width 5rem
                     height 1.3438rem
+                    padding 0 1.5625rem 0 0
                     border 0
                     background-color transparent
                     font-size 0
+                    color #7e8c8d
                     z-index 500
+                    direction rtl
                     option
-                        font-size 0.4063rem
+                        font-size 0.3438rem
+                .inputBox
+                    display flex
+                    position absolute
+                    right 1.625rem
+                    top 0.0313rem
+                    height 1.3438rem
+                    margin-top -0.0625rem
+                    border 0
+                    background-color transparent
+                    font-size 0.3438rem
+                    color #7e8c8d
+                    text-align right
+                    z-index 500
+                    input, span
+                        background-color transparent
+                    input
+                        margin 0.0313rem 0.0313rem 0 0
+                        color #7e8c8d
+                        text-align right
             .indent
                 border-bottom-none()               
             .line
